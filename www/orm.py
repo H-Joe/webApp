3b5fe27acc2d40fc820e8d5559dfa65bad442976
Qiao <luohq0321@sina.com>
2018-02-06 08:54:21 +0800

    文件修改

---
+++ www/orm.py
@@ -0,0 +1,13 @@
+import asyncio, logging
+import aiomysql
+
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+async def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306)
+    )
