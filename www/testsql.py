57482881ea1cc8dda890710a3f753056fbaf9f1a
Qiao <luohq0321@sina.com>
2018-02-11 11:15:19 +0800

    新增功能页面

---
+++ www/testsql.py
@@ -0,0 +1,15 @@
+
+
+import orm, asyncio
+from models import User
+
+loop = asyncio.get_event_loop()
+
+
+async def test():
+    await orm.create_pool(loop=loop, user='root', password='root', db='webAppDB')
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+    await u.save()
+
+
+loop.run_until_complete(test())
